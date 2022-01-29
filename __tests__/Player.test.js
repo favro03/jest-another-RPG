0a26ab6cfcb451ee974848980f6ed6c9a57ca4e2
tfavro <wetr9902@gmail.com>
2022-01-29 14:37:37 -0600

    added constructors/methods

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,10 +1,9 @@
+
 const Potion = require('../lib/Potion');
 
 jest.mock('../lib/Potion');
-console.log(new Potion());
 
 const Player = require('../lib/Player');
-const Ppotion = require('../lib/__mocks__/Potion');
 
 test('create a player object', () =>{
     const player = new Player('Dave');
@@ -14,3 +13,22 @@ test('create a player object', () =>{
     expect(player.agility).toEqual(expect.any(Number));
     expect(player.inventory).toEqual(expect.arrayContaining([expect.any(Object)]));
 });
+
+test("gets player's stats as an object", () => {
+    const player = new Player('Dave');
+  
+    expect(player.getStats()).toHaveProperty('potions');
+    expect(player.getStats()).toHaveProperty('health');
+    expect(player.getStats()).toHaveProperty('strength');
+    expect(player.getStats()).toHaveProperty('agility');
+  });
+
+  test('gets inventory from player or returns false', () => {
+    const player = new Player('Dave');
+  
+    expect(player.getInventory()).toEqual(expect.any(Array));
+  
+    player.inventory = [];
+  
+    expect(player.getInventory()).toEqual(false);
+  });
